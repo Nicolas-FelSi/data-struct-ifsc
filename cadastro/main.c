11fd89c393e26af8e22edd4169d1c10eb771bc55
nicolas <nicolasfelipe3702@gmail.com>
2024-07-29 16:02:56 -0300

    atualização exercicio

--- cadastro/main.c
+++ cadastro/main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
 
 typedef struct {
     int codigo;
@@ -15,6 +15,8 @@ int mostrar_menu(){
     printf(" 1. Incluir\n");
     printf(" 2. Listar\n");
     printf(" 3. Pesquisar\n");
+    printf(" 4. Gravar Arquivo\n");
+    printf(" 5. Ler Arquivo\n");
     printf(" 0. Sair\n");
     printf(" Tecle Opcao: ");
     scanf("%d", &opcao);
@@ -24,13 +26,18 @@ int mostrar_menu(){
 int inclusao(tProd* vProd, int qtd){
     system("cls");
     printf("Inclusao\n");
-    printf("codigo...:");
-    scanf("%d", &vProd[qtd].codigo);
+    //printf("codigo...:");
+    //scanf("%d", &vProd[qtd].codigo);
+    fflush(stdin);
+    vProd[qtd].codigo = qtd+1;
+
     printf("descricao..:");
     fflush(stdin);
+
     gets(vProd[qtd].descricao);
     printf("preco...:");
     scanf("%f", &vProd[qtd].preco);
+
     printf("Registro Incluido!\n");
     system("pause");
     return qtd+1;
@@ -76,6 +83,51 @@ int pesquisar(tProd* vProd, int qtd){
     system("pause");
 }
 
+int gravar_arquivo(tProd* vProd, int qtd) {
+    char nomeArquivo[] = "produtos.csv";
+    FILE* file = fopen(nomeArquivo, "w");
+    if(file == NULL) {
+        printf("Erro ao tentar gravar o arquivo\n");
+    } else {
+        for(int i=0; i<qtd; i++){
+            fprintf(file, "%d;%s;%f \n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
+        }
+    }
+    printf("Arquivo gravado com sucesso.\n");
+    system("pause");
+    fclose(file);
+}
+
+int ler_arquivo(tProd* vProd) {
+    int cont = 0;
+    char* token;
+    char linha[256] = "";
+    char nomeArquivo[] = "produtos.csv";
+    FILE* file = fopen(nomeArquivo, "r");
+    if(file == NULL) {
+        printf("Erro ao tentar ler o arquivo\n");
+    } else {
+        while(fgets(linha, 256, file)) {
+            printf("%s", linha);
+
+            token = strtok(linha, ";\n");
+            vProd[cont].codigo = atoi(token);
+
+            token = strtok(NULL, ";\n");
+            strcpy(vProd[cont].descricao, token);
+
+            token = strtok(NULL, ";\n");
+            vProd[cont].preco = atof(token);
+
+            cont++;
+        }
+        printf("Registro lidos: %d\n", cont);
+    }
+    fclose(file);
+    system("pause");
+    return cont;
+}
+
 int main()
 {
     tProd vProd[3];
@@ -94,6 +146,12 @@ int main()
                 break;
             case 3:
                 pesquisar(vProd, qtd);
+                break;
+            case 4:
+                gravar_arquivo(vProd, qtd);
+                break;
+            case 5:
+                qtd = ler_arquivo(vProd);
                 break;
             case 0:
                 printf("Encerrando...\n");
